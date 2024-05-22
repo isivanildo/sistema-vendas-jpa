497ba8beb41aab191d64b695975c03c0ab19a0d6
Ivanildo Ferreira <isivanildo@hotmail.com>
2024-05-21 21:01:19 -0300

    Iniciando o projeto

---
+++ src/main/java/br/com/belemtech/springjpasistemavenda/SpringJpaSistemaVendaApplication.java
@@ -0,0 +1,13 @@
+package br.com.belemtech.springjpasistemavenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaSistemaVendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaSistemaVendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/belemtech/springjpasistemavenda/SpringJpaSistemaVendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.belemtech.springjpasistemavenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaSistemaVendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
